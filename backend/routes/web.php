8269c036e5ca138b8ec317177b30152de7d69c44
Z10-1Z <ziadabdelaziz99@gmail.com>
2026-05-13 17:17:20 +0300

    use nixpacks

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response('OK', 200);
-});
+Route::get('/', fn () => response()->json([
+    'status' => 'running'
+ ]));
